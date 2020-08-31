cf65d90e571796655825aecae8eb0f42ef1d6cc9
SuL <jjw011202@naver.com>
2020-08-31 19:55:52 +0900

    LOOK_AT 패킷 오류 수정, 리플렉션을 이용하여 1.13+ 버전 지원, |pitch| < 90

--- src/main/java/me/sul/skrecoil/SkRecoil.java
+++ src/main/java/me/sul/skrecoil/SkRecoil.java
@@ -6,8 +6,11 @@ import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.IOException;
+import java.util.logging.Level;
 
 public final class SkRecoil extends JavaPlugin {
+    private static String OBC_PREFIX = Bukkit.getServer().getClass().getPackage().getName();
+    private static String NMS_PREFIX = OBC_PREFIX.replace("org.bukkit.craftbukkit", "net.minecraft.server");
 
     @Override
     public void onEnable() {
@@ -21,10 +24,16 @@ public final class SkRecoil extends JavaPlugin {
                 }
             }
         }
+        Bukkit.getLogger().log(Level.INFO, "[SkRecoil] §cSkRecoil§f이 활성화 되었습니다.");
+        Bukkit.getLogger().log(Level.INFO, "[SkRecoil] NMS version: " + NMS_PREFIX);
     }
 
     @Override
     public void onDisable() {
+        Bukkit.getLogger().log(Level.INFO, "§cSkRecoil§f이 비활성화 되었습니다.");
+    }
 
+    public static String getNMSPrefix() {
+        return NMS_PREFIX;
     }
 }
--- src/main/java/me/sul/skrecoil/effect/EffAddRecoil.java
+++ src/main/java/me/sul/skrecoil/effect/EffAddRecoil.java
@@ -9,6 +9,9 @@ import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.ProtocolLibrary;
 import com.comphenix.protocol.ProtocolManager;
 import com.comphenix.protocol.events.PacketContainer;
+import me.sul.skrecoil.ReflectionUtil;
+import me.sul.skrecoil.SkRecoil;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.util.Vector;
@@ -16,14 +19,17 @@ import org.bukkit.util.Vector;
 import javax.annotation.Nullable;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Objects;
+import java.util.logging.Level;
 
 public class EffAddRecoil extends Effect {
+    private final ProtocolManager pm = ProtocolLibrary.getProtocolManager();
+    private Class<?> anchorClazz;
     private Expression<Player> player;
     private Expression<Number> pitch;
     private Expression<Number> yaw;
 
     static {
-        Skript.registerEffect(EffAddRecoil.class, "add recoil %entity% with pitch %number% and yaw %number%");
+        Skript.registerEffect(EffAddRecoil.class, "add recoil %player% with pitch %number% and yaw %number%");
     }
 
     @SuppressWarnings("unchecked")
@@ -37,7 +43,7 @@ public class EffAddRecoil extends Effect {
 
     @Override
     public String toString(@Nullable Event event, boolean b) {
-        return getClass().toString();
+        return "add recoil " + player.toString(event, b) + " with pitch " + pitch.toString(event, b) + " and yaw " + yaw.toString(event, b);
     }
 
     @Override
@@ -45,27 +51,46 @@ public class EffAddRecoil extends Effect {
         addRecoil(Objects.requireNonNull(player.getSingle(event)), Objects.requireNonNull(yaw.getSingle(event)).doubleValue(), Objects.requireNonNull(pitch.getSingle(event)).doubleValue());
     }
 
-    private void addRecoil(Player p, double addToYaw, double addToPitch) { // 가로, 세로
-        double yaw = Math.toRadians(-p.getLocation().getYaw() - 90.0F + (-1)*addToYaw);
-        double pitch = Math.toRadians(-p.getLocation().getPitch() + (-1)*addToPitch);
-        if (Math.abs(pitch) > 180) pitch = (pitch >= 0) ? 180 : -180;
+    private void addRecoil(Player player, double addToYaw, double addToPitch) { // 가로, 세로
+        Vector vector = convert2Vector(player.getLocation().getYaw(), player.getLocation().getPitch(), addToYaw, addToPitch);
+        sendLookAtPacket(player, vector);
+    }
+
+    private Vector convert2Vector(double origYaw, double origPitch, double addToYaw, double addToPitch) {
+        double yaw = -origYaw - 90.0F + (-1)*addToYaw;
+        double pitch = -origPitch + (-1)*addToPitch;
+        yaw = yaw / 180.0 * Math.PI;
+        pitch = pitch / 180.0 * Math.PI;
+        if (Math.abs(pitch) > Math.PI / 2) {
+            pitch = (pitch >= 0) ? Math.PI / 2 : -1 * Math.PI / 2;
+            pitch -= Math.PI * 0.1/180;
+        }
 
         double x = Math.cos(pitch) * Math.cos(yaw);
         double y = Math.sin(pitch);
         double z = -Math.sin(yaw) * Math.cos(pitch);
-        sendLookAtPacket(p, new Vector(x, y, z));
+        return new Vector(x, y, z);
     }
 
-    private void sendLookAtPacket(Player p, Vector vector) {
-        ProtocolManager pm = ProtocolLibrary.getProtocolManager();
-        final PacketContainer lookAt = pm.createPacket(PacketType.Play.Server.LOOK_AT, false);
-        lookAt.getIntegers().write(0, 1);
-        lookAt.getDoubles().write(0, p.getEyeLocation().getX() + vector.getX());
-        lookAt.getDoubles().write(1, p.getEyeLocation().getY() + vector.getY());
-        lookAt.getDoubles().write(2, p.getEyeLocation().getZ() + vector.getZ());
-        lookAt.getBooleans().write(0, false);
+    private void sendLookAtPacket(Player player, Vector vector) {
+        if (vector.getX() == 0 && vector.getY() == 0 && vector.getZ() == 0) return;
+        PacketContainer lookAtPacket = pm.createPacket(PacketType.Play.Server.LOOK_AT, false);
+        if (anchorClazz == null) {
+            try {
+                anchorClazz = Class.forName(SkRecoil.getNMSPrefix() + ".ArgumentAnchor$Anchor");
+            } catch (ClassNotFoundException e) {
+                Bukkit.getLogger().log(Level.WARNING, "Failed to get " + SkRecoil.getNMSPrefix() + ".ArgumentAnchor$Anchor Class");
+                e.printStackTrace();
+            }
+        }
+        Object enumArgumentAnchor_EYES = ReflectionUtil.getEnumConstant(anchorClazz, "EYES");
+        lookAtPacket.getModifier().write(4, enumArgumentAnchor_EYES);
+        lookAtPacket.getDoubles().write(0, player.getEyeLocation().getX() + vector.getX());
+        lookAtPacket.getDoubles().write(1, player.getEyeLocation().getY() + vector.getY());
+        lookAtPacket.getDoubles().write(2, player.getEyeLocation().getZ() + vector.getZ());
+        lookAtPacket.getBooleans().write(0, false);
         try {
-            pm.sendServerPacket(p, lookAt);
+            pm.sendServerPacket(player, lookAtPacket);
         } catch (InvocationTargetException e) {
             e.printStackTrace();
         }
